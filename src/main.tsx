76e665b4d92642910dac040d5573ac4204191c33
wei-zhe <weizheweng@google.com>
2024-12-28 20:30:32 +0800

    chore: update ts library and adjust a part of settings

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App'
-import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
-import theme from './styles/theme'
 
 const target = document.getElementById('root')
 if (target == null) throw new Error()
 
 ReactDOM.createRoot(target).render(
   <React.StrictMode>
-    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-    <ChakraProvider theme={theme}>
-      <App />
-    </ChakraProvider>
+    <App />
   </React.StrictMode>
 )
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
+import checker from 'vite-plugin-checker'
 import { resolve } from 'path'
 
 const ENV_PREFIX = 'REACT_APP_'
@@ -9,8 +10,7 @@ const ENV_PREFIX = 'REACT_APP_'
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, 'env', ENV_PREFIX)
   return {
-    // base: '/movies/',
-    plugins: [react(), svgr()],
+    plugins: [react(), svgr(), checker({ typescript: true })],
     server: {
       port: 3000,
       open: env.SERVER_OPEN_BROWSER === 'true',
